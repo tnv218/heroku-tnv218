955347943d855c1f1da3030bee60f15476d619bd
tnv218 <tnv218@gmail.com>
2021-12-27 21:12:49 +0700

    u

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello World from tnv218');
+// web server
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+// listen on port 3000
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
